204e9e2e875ccc5c806cfafd28d36a7cc5457f7a
Marianna Nishovych <nisovicmarianna@gmail.com>
2024-09-06 11:08:23 +0300

    add third commit

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,12 +2,51 @@
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {
+let formData = {
     email: "",
     message: "",
 };
 
 const form = document.querySelector('.feedback-form');
-console.log(form);
-const textarea = document.querySelector('textarea');
-console.log(textarea);
+
+
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+    event.preventDefault();
+
+    const message = event.currentTarget.elements.message.value.trim();
+    const email = event.currentTarget.elements.email.value.trim();
+
+    if (!email || !message) {
+        return alert('Fill please all fields');
+    };
+
+    console.log(formData);
+
+    localStorage.removeItem(STORAGE_KEY);
+    form.reset();
+};
+
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput (event) {
+    formData = {  
+    email: form.elements.email.value.trim(),  
+    message: form.elements.message.value.trim(),  
+}; 
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
+};
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));  
+if (savedData) {  
+formData = savedData;  
+form.elements.email.value = formData.email;  
+form.elements.message.value = formData.message;  
+} 
+
+
+
